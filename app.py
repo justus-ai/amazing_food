6fbb696d449d647803b71bf5fc89de1063cec2f4
Justus marwa <justusmwita@gmail.com>
2021-11-24 11:26:08 +0000

    Create the Flask App

---
+++ app.py
@@ -0,0 +1,17 @@
+import os 
+from flask import Flask
+if os.path.exists("env.py"):
+    import env
+
+
+    app= Flask(__name__)
+
+    @app.route("/")
+    def delicious():
+        return "How delicious"
+
+
+    if __name__== "_main_":
+        app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
