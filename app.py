d44eda64182eeb2e04665b802770a09ee9700ef5
Justus marwa <justusmwita@gmail.com>
2021-11-29 20:57:20 +0000

    changed the import os to small letters to help the app run

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
-import os 
+import os
+
 from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectid import objectid
 if os.path.exists("env.py"):
     import env
 
+app = Flask(__name__)
 
-    app = Flask(__name__)
-
-    @app.route("/")
-    def delicious():
-        return "How delicious"
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.envriron.get("SECRET_KEY")
 
 
-    if __name__== "_main_":
-        app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
-            debug=True)
+@app.route("/")
+def delicious():
+     return "How delicious"
+
+
+if __name__== "_main_":
+    app.run(host=os.environ.get("IP"),
+           port=int(os.environ.get("PORT")),
+        debug=False)
