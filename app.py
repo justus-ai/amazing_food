9ab345a970c9baf9e2867b33bef0746107a76f83
Justus marwa <justusmwita@gmail.com>
2022-01-03 10:18:42 +0000

    Create the Flask App

--- app.py
+++ app.py
@@ -1,24 +1,18 @@
 import os
-
 from flask import Flask
-from flask_pymongo import PyMongo
-from bson.objectid import objectid
 if os.path.exists("env.py"):
     import env
 
-app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.secret_key = os.envriron.get("SECRET_KEY")
+app = Flask(__name__)
 
 
 @app.route("/")
-def delicious():
-     return "How delicious"
+def hello():
+    return "Hello World ... again!"
 
 
-if __name__== "_main_":
+if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-           port=int(os.environ.get("PORT")),
-        debug=False)
+            port=int(os.environ.get("PORT")),
+            debug=True)
